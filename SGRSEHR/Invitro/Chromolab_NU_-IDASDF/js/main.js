5e5adb8980b33d4174c1002da4e863d57154b944
invitroru <95089276+invitroru@users.noreply.github.com>
2022-05-28 02:41:26 +0500

    Delete SGRSEHR directory

--- SGRSEHR/Invitro/Chromolab_NU_-IDASDF/js/main.js
+++
@@ -1,7 +0,0 @@
-const menuButton = document.querySelector('.menu');
-if ( menuButton ) {
-	const menuBody = document.querySelector('.menu__list');
-	menuButton.addEventListener("click", function(e) {
-		menuBody.classList.toggle('active');
-	});
-}
